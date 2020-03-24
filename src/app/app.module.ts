bf155787ac90f9aaf4eb0b4e7cdd8b51e6671890
Pablo Grant <grantpablo4@gmail.com>
2020-03-23 23:55:21 -0500

    updating attolink

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -7,13 +7,15 @@ import { MdcModule } from './mdc/mdc.module';
 import { FeaturesComponent } from './features/features.component';
 import { FaqComponent } from './faq/faq.component';
 import { DocumentationComponent } from './documentation/documentation.component';
+import { WelcomePageComponent } from './welcome-page/welcome-page.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     FeaturesComponent,
     FaqComponent,
-    DocumentationComponent
+    DocumentationComponent,
+    WelcomePageComponent
   ],
   imports: [
     BrowserModule,
--- src/app/mdc/mdc.module.ts
+++ src/app/mdc/mdc.module.ts
@@ -1,6 +1,6 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
-import { MdcTopAppBarModule, MdcTypographyModule, MdcListModule} from '@angular-mdc/web';
+import { MdcTopAppBarModule, MdcTypographyModule, MdcListModule, MdcIconModule,} from '@angular-mdc/web';
 import { MdcButtonModule } from '@angular-mdc/web';
 
 
@@ -12,12 +12,14 @@ import { MdcButtonModule } from '@angular-mdc/web';
     MdcButtonModule,
     MdcListModule,
     MdcTopAppBarModule,
+    MdcIconModule,
     MdcTypographyModule,
 
   ],
   exports: [
     MdcButtonModule,
     MdcListModule,
+    MdcIconModule,
     MdcTypographyModule,
     MdcTopAppBarModule,
 

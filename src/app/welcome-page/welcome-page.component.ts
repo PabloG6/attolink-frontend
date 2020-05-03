a918b3c84b5049c1b100191449b35b4f6adc6998
Pablo Grant <grantpablo4@gmail.com>
2020-05-03 03:33:50 -0500

    moved the landing page to the welcome page and changed app.component.html to be a router-outlet

--- src/app/welcome-page/welcome-page.component.ts
+++ src/app/welcome-page/welcome-page.component.ts
@@ -1,15 +1,44 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, OnDestroy } from '@angular/core';
+import { Router, ActivatedRoute, Params } from '@angular/router';
+import { MdcDialog } from '@angular-mdc/web';
+import { LoginModalComponent } from '../login-modal/login-modal.component';
+import { Observable, Subscription } from 'rxjs';
 
 @Component({
   selector: 'app-welcome-page',
   templateUrl: './welcome-page.component.html',
   styleUrls: ['./welcome-page.component.scss']
 })
-export class WelcomePageComponent implements OnInit {
+export class WelcomePageComponent implements OnInit, OnDestroy {
+  private _subscriptions: Subscription[] = [];
+  
+  constructor(private _router: Router, private _activatedRoute: ActivatedRoute, private _dialog: MdcDialog) { }
 
-  constructor() { }
 
   ngOnInit(): void {
+    let dialogRef;
+    const activatedRoute$ = this._activatedRoute.queryParams.subscribe((queryParams: Params) => {
+      if(queryParams["login"]) {
+        dialogRef = this._dialog.open(LoginModalComponent, )
+      }
+
+    });
+
+    dialogRef.
+    this._subscriptions.push(activatedRoute$);
+
+
+  }
+
+  public login() {
+  }
+
+  public signup() {
+    this._router.navigate(["/signup"]);
+  }
+
+  ngOnDestroy() {
+    this._subscriptions.forEach((subscription) => subscription.unsubscribe())
   }
 
 }

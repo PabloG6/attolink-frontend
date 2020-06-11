5a2eca51b985dce0503dcb31aef14a1238f1c81f
Pablo Grant <grantpablo4@gmail.com>
2020-06-11 01:54:32 -0500

    made demo partially functional. need to add an iframe

--- src/app/demo/demo.component.ts
+++ src/app/demo/demo.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import { HttpClient, HttpHeaders } from '@angular/common/http';
+import { FormControl } from '@angular/forms';
 
 @Component({
   selector: 'app-demo',
@@ -7,9 +9,24 @@ import { Component, OnInit } from '@angular/core';
 })
 export class DemoComponent implements OnInit {
 
-  constructor() { }
+  demoControl: FormControl = new FormControl();
+  constructor(private _http: HttpClient
+  ) { }
 
   ngOnInit(): void {
+  }
+
+  displayPreview() {
+
+  const url = this.demoControl.value || 'https://example.com'; 
+  console.log(this.demoControl);
+  this._http.get('/v1/preview', {
+    params: {url: url},
+
+    headers: new HttpHeaders().append('apikey', 'ddaf02d1-11cd-49c0-a4e6-3a13976315d3',) 
+  }).subscribe((response) => {
+    console.log(response);
+  })
   }
 
 }
